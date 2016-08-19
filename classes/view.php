a023566b06831cec8683274271c66504e0379a5b
shibata <shibata@jidaikobo.com>
2016-08-19 20:32:38 +0900

    add view

---
+++ classes/view.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * A11yc\View
+ *
+ * @package    part of A11yc
+ * @version    1.0
+ * @author     Jidaikobo Inc.
+ * @license    WTFPL2.0
+ * @copyright  Jidaikobo Inc.
+ * @link       http:/www.jidaikobo.com
+ */
+namespace A11yc;
+class View extends \Kontiki\View
+{
+}
