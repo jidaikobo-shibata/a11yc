a8388b11b21bea561034c2310f7af8cc7eb20810
shibata <shibata@jidaikobo.com>
2016-08-20 19:28:40 +0900

    add kontiki

---
+++ config/kontiki.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * config
+ *
+ * @package    part of Kontiki
+ */
+
+// database - sqlite or mysql
+define('KONTIKI_DBTYPE', 'sqlite');
+
+// sqlite
+define('KONTIKI_SQLITE_PATH', dirname(__DIR__).'/db/db.sqlite');
+
+// mysql
+define('KONTIKI_MYSQL_NAME', '');
+define('KONTIKI_MYSQL_USER', '');
+define('KONTIKI_MYSQL_HOST', '');
+define('KONTIKI_MYSQL_PASSWORD', '');
+
+// view
+define('KONTIKI_VIEW_PATH', '/path/to/templates');
