b2388c3df1e2a3a0075b033b85910a85381e343a
有松 <arimatsu@jidaikobo.com>
2016-08-28 15:23:17 +0900

    temp

--- libs/a11yc/views/setup/index.php
+++
@@ -1,7 +0,0 @@
-<h1><?php echo A11YC_LANG_SETUP_TITLE ?></h1>
-<form action="" method="POST">
-<?php echo $form; ?>
-	<div id="a11y_submit">
-		<input type="submit" value="<?php echo A11YC_LANG_SETUP_SUBMIT ?>" />
-	</div>
-</form>
