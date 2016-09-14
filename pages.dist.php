35bec7e97790c06cd9b37b7c9685189f98413f7e
shibata <shibata@jidaikobo.com>
2016-09-14 18:00:36 +0900

    fix sample of report and pages

--- pages.dist.php
+++ pages.dist.php
@@ -10,10 +10,23 @@
  * @link       http:/www.jidaikobo.com
  */
 
-include (__DIR__.'/inc_report.php');
+// kontiki
+define('KONTIKI_CONFIG_PATH', __DIR__.'/config/kontiki.php');
+require (__DIR__.'/libs/kontiki/main.php');
 
-// re assign
-$title           = A11YC_LANG_CHECKED_PAGES;
+// a11yc
+require (__DIR__.'/config/config.php');
+require (A11YC_PATH.'/main.php');
+
+// database
+\A11yc\Db::forge();
+\A11yc\Db::init_table();
+
+// pages
+$pages = \A11yc\Db::fetch_all('SELECT * FROM '.A11YC_TABLE_PAGES.' WHERE `trash` = 0 ORDER BY `url` ASC;');
+
+// title
+$title = A11YC_LANG_CHECKED_PAGES;
 
 ?><!DOCTYPE html>
 <html lang="<?php echo A11YC_LANG ?>">
@@ -40,11 +53,27 @@ $title           = A11YC_LANG_CHECKED_PAGES;
 
 <h1><?php echo $title ?></h1>
 
+<table>
+<thead>
+<tr>
+	<th><?php echo A11YC_LANG_CHECKLIST_TARGETPAGE ?></th>
+	<th><?php echo A11YC_LANG_CURRENT_LEVEL ?></th>
+	<th><?php echo A11YC_LANG_TEST_DATE ?></th>
+</tr>
+</thead>
 <?php
-foreach ($passed_pages as $v):
-
-endforeach;
+foreach ($pages as $v):
+$url = \A11yc\Util::s($v['url']);
 ?>
+<tr>
+	<td><?php echo '<a href=".$url.">'.$url.'</a>' ?></td>
+	<td><?php echo \A11yc\Util::num2str($v['level']) ?></td>
+	<td><?php echo \A11yc\Util::s($v['date']) ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
+
+<p style="text-align: right;"><a href="./report.dist.php"><?php echo A11YC_LANG_REPORT ?></a></p>
 
 </body>
 </html>
