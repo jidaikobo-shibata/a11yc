c91f270faa466c5384bd5850980a546dcdc68bd1
shibata <shibata@jidaikobo.com>
2016-09-22 19:00:37 +0900

    change place constant

--- libs/a11yc/main.php
+++ libs/a11yc/main.php
@@ -10,6 +10,23 @@
  * @link       http:/www.jidaikobo.com
  */
 
+// path
+define('A11YC_PATH', dirname(__DIR__).'/libs/a11yc');
+define('A11YC_CLASSES_PATH',  A11YC_PATH.'/classes');
+define('A11YC_RESOURCE_PATH', A11YC_PATH.'/resources/'.A11YC_LANG);
+
+// tables
+define('A11YC_TABLE_PAGES',  'a11y_pages');
+define('A11YC_TABLE_CHECKS', 'a11y_checks');
+define('A11YC_TABLE_SETUP',  'a11y_setup');
+define('A11YC_TABLE_BULK',   'a11y_bulk');
+
+// url
+define('A11YC_BULK_URL',      A11YC_URL.'?c=bulk&amp;a=index');
+define('A11YC_PAGES_URL',     A11YC_URL.'?c=pages&amp;a=index');
+define('A11YC_CHECKLIST_URL', A11YC_URL.'?c=checklist&amp;a=index&amp;url=');
+define('A11YC_DOC_URL',       A11YC_URL.'?c=docs&amp;a=each&amp;code=');
+
 // languages
 include A11YC_PATH.'/languages/'.A11YC_LANG.'.php';
 
