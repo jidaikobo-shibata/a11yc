9d9d36fc3c9e7573448d291d389f1c57be159fdb
shibata <shibata@jidaikobo.com>
2016-09-27 16:43:21 +0900

    fix define

--- libs/a11yc/main.php
+++ libs/a11yc/main.php
@@ -11,20 +11,20 @@
  */
 
 // path
-define('A11YC_CLASSES_PATH',  A11YC_PATH.'/classes');
-define('A11YC_RESOURCE_PATH', A11YC_PATH.'/resources/'.A11YC_LANG);
+defined('A11YC_CLASSES_PATH')  or define('A11YC_CLASSES_PATH',  A11YC_PATH.'/classes');
+defined('A11YC_RESOURCE_PATH') or define('A11YC_RESOURCE_PATH', A11YC_PATH.'/resources/'.A11YC_LANG);
 
 // tables
-define('A11YC_TABLE_PAGES',  'a11y_pages');
-define('A11YC_TABLE_CHECKS', 'a11y_checks');
-define('A11YC_TABLE_SETUP',  'a11y_setup');
-define('A11YC_TABLE_BULK',   'a11y_bulk');
+defined('A11YC_TABLE_PAGES')  or define('A11YC_TABLE_PAGES',  'a11y_pages');
+defined('A11YC_TABLE_CHECKS') or define('A11YC_TABLE_CHECKS', 'a11y_checks');
+defined('A11YC_TABLE_SETUP')  or define('A11YC_TABLE_SETUP',  'a11y_setup');
+defined('A11YC_TABLE_BULK')   or define('A11YC_TABLE_BULK',   'a11y_bulk');
 
 // url
-define('A11YC_BULK_URL',      A11YC_URL.'?c=bulk&amp;a=index');
-define('A11YC_PAGES_URL',     A11YC_URL.'?c=pages&amp;a=index');
-define('A11YC_CHECKLIST_URL', A11YC_URL.'?c=checklist&amp;a=index&amp;url=');
-define('A11YC_DOC_URL',       A11YC_URL.'?c=docs&amp;a=each&amp;code=');
+defined('A11YC_BULK_URL')      or define('A11YC_BULK_URL',      A11YC_URL.'?c=bulk&amp;a=index');
+defined('A11YC_PAGES_URL')     or define('A11YC_PAGES_URL',     A11YC_URL.'?c=pages&amp;a=index');
+defined('A11YC_CHECKLIST_URL') or define('A11YC_CHECKLIST_URL', A11YC_URL.'?c=checklist&amp;a=index&amp;url=');
+defined('A11YC_DOC_URL')       or define('A11YC_DOC_URL',       A11YC_URL.'?c=docs&amp;a=each&amp;code=');
 
 // languages
 include A11YC_PATH.'/languages/'.A11YC_LANG.'.php';
