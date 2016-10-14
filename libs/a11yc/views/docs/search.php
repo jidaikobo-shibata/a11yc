cc16d9499f33afcb259c5b0d35fa2cfe14342508
shibata <shibata@jidaikobo.com>
2016-10-14 15:03:52 +0900

    fix constant

--- libs/a11yc/views/docs/search.php
+++ libs/a11yc/views/docs/search.php
@@ -1,6 +1,6 @@
 <form id="a11yc_docs_search" action="<?php echo A11YC_URL ?>" method="GET">
 	<input type="hidden" name="c" value="docs">
 	<input type="hidden" name="a" value="index">
-	<label class="a11yc_skip" for="a11yc_str"><?php echo A11YC_LANG_DOCS_SEARCH ?></label><input type="text" name="s" id="a11yc_str" size="24" value="<?php echo $word ?>">
-	<input type="submit" value="<?php echo A11YC_LANG_DOCS_SEARCH ?>">
+	<label class="a11yc_skip" for="a11yc_str"><?php echo A11YC_LANG_CTRL_KEYWORD_TITLE ?></label><input type="text" name="s" id="a11yc_str" size="24" value="<?php echo $word ?>">
+	<input type="submit" value="<?php echo A11YC_LANG_CTRL_SEARCH ?>">
 </form>
