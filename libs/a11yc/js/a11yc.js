b6ba6c33fa5b303194e2c99bdaeb014322761b30
有松 <arimatsu@jidaikobo.com>
2016-11-30 20:02:49 +0900

    js

--- libs/a11yc/js/a11yc.js
+++ libs/a11yc/js/a11yc.js
@@ -319,7 +319,7 @@ if($('.a11yc_table_check')[0])
 	var c_id = $('#a11yc_checks').data('a11ycCurrentUser');
 	$('#a11yc_checks :checkbox').on('click', function(){
 		var select = $(this).closest('tr').find('select');
-		if(c_id !== select.val()) select.val(c_id).a11yc_flash();
+		if(String(c_id) !== select.val()) select.val(c_id).a11yc_flash();
 	});
 	// highlight/adjust position when checkbox is clicked
 	$('#a11yc_checks :checkbox').on('click', function(){
