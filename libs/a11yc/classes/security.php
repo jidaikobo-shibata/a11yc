269b6e5a0202e13f2f5a353e1e3e8c9341434d8e
shibata <shibata@jidaikobo.com>
2016-12-15 20:50:07 +0900

    add Security check

---
+++ libs/a11yc/classes/security.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A11yc\Security
+ *
+ * @package    part of A11yc
+ * @version    1.0
+ * @author     Jidaikobo Inc.
+ * @license    The MIT License (MIT)
+ * @copyright  Jidaikobo Inc.
+ * @link       http://www.jidaikobo.com
+ */
+namespace A11yc;
+class Security
+{
+	/**
+	 * _init
+	 *
+	 * @return  void
+	 */
+	public static function _init()
+	{
+	}
+}
