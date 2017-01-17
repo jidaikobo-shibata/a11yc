8ffcfab86127e4ef974401c40586954f544313d9
shibata <shibata@jidaikobo.com>
2017-01-17 22:13:26 +0900

    remove p

--- libs/a11yc/views/disclosure/inc_related.php
+++ libs/a11yc/views/disclosure/inc_related.php
@@ -1,9 +1,9 @@
 <?php if ( ! $is_center): ?>
 <h2><?php echo A11YC_LANG_RELATED ?></h2>
 <ul>
-	<li><?php echo '<p class="a11yc_link"><a href="'.$policy_link.'">'.A11YC_LANG_POLICY.'</a></p>'; ?></li>
+	<li><?php echo '<a href="'.$policy_link.'">'.A11YC_LANG_POLICY.'</a>'; ?></li>
 	<?php if ($setup['selected_method'] !== 0 && isset($is_total) && $is_total == FALSE):  ?>
-		<li><?php echo '<p class="a11yc_link"><a href="'.$report_link.'">'.A11YC_LANG_REPORT.'</a></p>'; ?></li>
+		<li><?php echo '<a href="'.$report_link.'">'.A11YC_LANG_REPORT.'</a>'; ?></li>
 	<?php endif;  ?>
 </ul>
 <?php endif;  ?>
