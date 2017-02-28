17c14f04768b72f1469ded276fabf27cabd38cc8
shibata <shibata@jidaikobo.com>
2017-02-28 22:17:57 +0900

    add readme template

---
+++ libs/a11yc/views/post/readme.php
@@ -0,0 +1,34 @@
+<p><?php echo A11YC_LANG_POST_HOWTO ?></p>
+
+<h2><?php echo A11YC_LANG_POST_SERVICE_NAME_TITLE ?></h2>
+<p><?php echo A11YC_LANG_POST_SERVICE_NAME_EXP ?></p>
+
+<h2><?php echo A11YC_LANG_POST_CONDITION_TITLE ?></h2>
+<ul>
+	<li><?php echo A11YC_LANG_POST_CONDITION_EXP_FREE ?></li>
+	<li><?php echo A11YC_LANG_POST_CONDITION_EXP_10MIN ?></li>
+	<li><?php echo A11YC_LANG_POST_CONDITION_EXP_24H ?></li>
+</ul>
+
+<h2><?php echo A11YC_LANG_POST_COLLECTION_TITLE ?></h2>
+<ul>
+	<li><?php echo A11YC_LANG_POST_COLLECTION_GOOGLE ?></li>
+	<li><?php echo A11YC_LANG_POST_COLLECTION_IP ?></li>
+</ul>
+
+<h2><?php echo A11YC_LANG_POST_VENDOR_TITLE ?></h2>
+<ul>
+	<li><?php echo A11YC_LANG_POST_VENDOR_JIDAIKOBO ?></li>
+	<li><?php echo A11YC_LANG_POST_VENDOR_JIDAIKOBO_TWITTER ?></li>
+</ul>
+
+<h2><?php echo A11YC_LANG_POST_TECH_TITLE ?></h2>
+<ul>
+	<li><?php echo A11YC_LANG_POST_TECH_A11YC ?></li>
+	<li><?php echo A11YC_LANG_POST_TECH_A11YC_ADD ?></li>
+	<li><?php echo A11YC_LANG_POST_TECH_JWP_A11YC ?></li>
+	<li><?php echo A11YC_LANG_POST_TECH_JWP_A11YC_ADD ?></li>
+</ul>
+
+<h2><?php echo A11YC_LANG_POST_FEEDBACK_TITLE ?></h2>
+<p><?php echo A11YC_LANG_POST_FEEDBACK_EXP ?></p>
