e876268cd9143e4e5f71e940e1b1cf81709e403d
有松 <arimatsu@jidaikobo.com>
2018-01-04 19:27:55 +0900

    a11yc_live css and js

--- assets/js/a11yc_live.js
+++ assets/js/a11yc_live.js
@@ -1,2 +1,14 @@
 jQuery(function($){
+	// overlay for disable buttons
+	$('body').append('<div id="a11yc_overlay">');
+
+	// alt
+	$('img').each(function(){
+		$(this).wrap('<span class="a11yc_live_img_wrapper">');
+		$('<span class="a11yc_live_alt">').text('alt="'+$(this).attr('alt')+'"').insertBefore(this);
+	});
+	
+	// movable labels ?
+	$labels = $('.a11yc_validation_code_error');
+
 });
