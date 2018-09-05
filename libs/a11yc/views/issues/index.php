8173fc2e2cf8448a40c77cd999bf507bb078cd77
有松 <arimatsu@jidaikobo.com>
2018-09-05 19:04:40 +0900

    add nl2br to error_message of issue

--- libs/a11yc/views/issues/index.php
+++ libs/a11yc/views/issues/index.php
@@ -9,7 +9,7 @@
 	<td><a href="<?php echo A11YC_CHECKLIST_URL.Util::urlenc($url) ?>" class="a11yc_hasicon"><span class="a11yc_skip"><?php echo A11YC_LANG_PAGES_CHECK ?></span><span class="a11yc_icon_check a11yc_icon_fa" role="presentation" aria-hidden="true"></span></a></td>
 	<td class="a11yc_issue_data"><ul>
 	<?php foreach ($pages as $page): ?>
-		<li><a href="<?php echo A11YC_REF_WCAG20_TECH_URL.$page['code'].'.html' ?>"><?php echo $yml[$page['code']]['title'] ?></a></li>
+		<li><a href="<?php echo A11YC_REF_WCAG20_TECH_URL.$page['code'].'.html' ?>"><?php echo nl2br($yml[$page['code']]['title']) ?></a></li>
 	<?php endforeach; ?>
 	</ul></td>
 </tr>
@@ -44,7 +44,7 @@ foreach ($issues as $status => $issue):
 							'<span class="a11yc_validation_code_notice">NOTICE</span>':
 							'<span class="a11yc_validation_code_error">ERROR!</span>';
 		?>
-			<li><?php echo $type ?><a href="<?php echo A11YC_ISSUES_VIEW_URL.intval($each_issue['id']) ?>"><?php echo $each_issue['id'].': '.Util::s($each_issue['error_message']) ?></a>
+			<li><?php echo $type ?><a href="<?php echo A11YC_ISSUES_VIEW_URL.intval($each_issue['id']) ?>"><?php echo $each_issue['id'].': '.nl2br(Util::s($each_issue['error_message'])) ?></a>
 
 			 (<?php if ($each_issue['trash'] != 0): ?>
 				<a href="<?php echo A11YC_ISSUES_UNDELETE_URL.intval($each_issue['id']) ?>"><?php echo A11YC_LANG_PAGES_UNDELETE ?></a><?php echo ' - '; ?>
