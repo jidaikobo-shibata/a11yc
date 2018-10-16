755cce1f00b8ed08137f0363b944e56ee9d5b3ee
shibata <shibata@jidaikobo.com>
2018-10-16 18:29:23 +0900

    add procedure crawling

--- libs/a11yc/classes/Controller/Tech.php
+++ libs/a11yc/classes/Controller/Tech.php
@@ -27,22 +27,150 @@ class Tech
 	public static function actionImport()
 	{
 		// physical path of Techniques for WCAG 2.0 complete.html
-		$path = '/Users/FOOBAR/Desktop/complete.html';
+		$path = '/Users/jidaikobo/Desktop/complete.html';
 		if ( ! file_exists($path)) Util::error();
 		$text = file_get_contents($path);
-		$text = preg_replace('/\<p[^\>]*?\>.+?\<\/p\>/mis', '', $text);
-		$techs = explode('<h3>', $text);
 
+		// get procedures
+		$techs = explode('<h3>', $text);
+		$procedures = self::getProcedures($techs);
+
+		// get title, type and criterions
+		$text4tech = preg_replace('/\<p[^\>]*?\>.+?\<\/p\>/mis', '', $text);
+		$techs = explode('<h3>', $text4tech);
+		$results = self::getTitles($techs);
+
+		// reverse value
+		$results2 = self::getReversedValue($results);
+
+		// output
+		$yml = self::outputYaml($results);
+		$yml2 = self::outputYaml($results2);
+		$yml3 = self::outputYaml($procedures);
+		die();
+	}
+
+	/**
+	 * outputYaml
+	 *
+	 * @param Array $results
+	 * @return Void
+	 */
+	private static function outputYaml($results)
+	{
+		if ( ! class_exists('Spyc'))
+		{
+			include A11YC_LIB_PATH.'/spyc/Spyc.php';
+		}
+		$yml = \Spyc::YAMLDump($results);
+		echo '<textarea style="width:100%;height:200px;background-color:#fff;color:#111;font-size:90%;font-family:monospace;position:relative;z-index:9999">';
+		echo $yml;
+		echo '</textarea>';
+	}
+
+	/**
+	 * get each
+	 *
+	 * @param String $tech
+	 * @return Array|false
+	 */
+	private static function getEach($tech)
+	{
+		if (substr($tech, 0, 7) !== '<a name') return false;
+		$each = explode('</h3>', $tech);
+		if (count($each) !== 2) return false;
+		return $each;
+	}
+
+	/**
+	 * get tech code
+	 *
+	 * @param Array $each
+	 * @return String|false
+	 */
+	private static function getTechCode($each)
+	{
+		if ( ! preg_match('/".+?"/', $each[0], $m)) return false;
+		$code = str_replace('"', '', $m[0]);
+		return $code;
+	}
+
+	/**
+	 * get Procedures
+	 *
+	 * @param Array $techs
+	 * @return Array
+	 */
+	private static function getProcedures($techs)
+	{
+		// remove introduction
+		unset($techs[0]);
+
+		$procedures = array();
+		foreach ($techs as $tech)
+		{
+			$each = self::getEach($tech);
+			if ($each === false) continue;
+
+			// tech code
+			$code = self::getTechCode($each);
+			if ($code === false) continue;
+
+			// code must not be contain "_".
+			if (strpos($code, '_') !== false) continue;
+
+			// needless type of criterion
+			if (strpos($code, 'FLASH') !== false) continue;
+			if (strpos($code, 'SL') !== false) continue;
+			if (strpos($code, 'PDF') !== false) continue;
+
+			// extract procedure
+			$target = '<h5 class="small-head" id="'.$code.'-procedure">';
+			$txt = substr($each[1], strpos($each[1], $target));
+
+			// needless str
+			$search = '</div></div><div class="technique"><hr class="divider" title="Beginning of new technique"/>';
+			$txt = str_replace($search, '', $txt);
+
+			// get procedure and result
+			$txts = explode('<h5', $txt);
+
+			// procedure not exist. C18, SCR36.
+			if ( ! isset($txts[2])) continue;
+
+			// extract text
+			$procedure = mb_substr($txts[1], mb_strpos($txts[1], '</h5>') + 5);
+			$result = mb_substr($txts[2], mb_strpos($txts[2], '</h5>') + 5);
+			$result = mb_substr($result, 0, mb_strpos($result, '</ul>') + 5);
+
+			$procedures[$code]['procedure'] = $procedure;
+			$procedures[$code]['result'] = $result;
+		}
+		return $procedures;
+	}
+
+	/**
+	 * get titles
+	 *
+	 * @param Array $techs
+	 * @return Array
+	 */
+	private static function getTitles($techs)
+	{
 		$results = array();
 		foreach ($techs as $tech)
 		{
-			if (substr($tech, 0, 7) !== '<a name') continue;
-			$each = explode('</h3>', $tech);
-			if (count($each) !== 2) continue;
+			$each = self::getEach($tech);
+			if ($each === false) continue;
 
 			// code
-			if ( ! preg_match('/".+?"/', $each[0], $m)) continue;
-			$code = str_replace('"', '', $m[0]);
+			$code = self::getTechCode($each);
+			if ($code === false) continue;
+
+			// needless type of criterion
+			if (strpos($code, 'FLASH') !== false) continue;
+			if (strpos($code, 'SL') !== false) continue;
+			if (strpos($code, 'PDF') !== false) continue;
 
 			// allowed types
 			$types = Values::techsTypes();
@@ -66,8 +194,17 @@ class Tech
 				'apps' => $apps,
 			);
 		}
+		return $results;
+	}
 
-		// reverse value
+	/**
+	 * get reversed value
+	 *
+	 * @param Array $results
+	 * @return Array
+	 */
+	private static function getReversedValue($results)
+	{
 		$results2 = array();
 		foreach ($results as $k => $v)
 		{
@@ -84,20 +221,6 @@ class Tech
 			}
 		}
 		ksort($results2);
-
-		if ( ! class_exists('Spyc'))
-		{
-			include A11YC_LIB_PATH.'/spyc/Spyc.php';
-		}
-		$yml = \Spyc::YAMLDump($results);
-		$yml2 = \Spyc::YAMLDump($results2);
-
-		echo '<textarea style="width:100%;height:200px;background-color:#fff;color:#111;font-size:90%;font-family:monospace;position:relative;z-index:9999">';
-		echo $yml;
-		echo '</textarea>';
-		echo '<textarea style="width:100%;height:200px;background-color:#fff;color:#111;font-size:90%;font-family:monospace;position:relative;z-index:9999">';
-		echo $yml2;
-		echo '</textarea>';
-		die();
+		return $results2;
 	}
 }
