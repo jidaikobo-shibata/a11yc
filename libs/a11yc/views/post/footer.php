da3a14c4706b1e92384e06ba47d61cfcc8151530
shibata <shibata@jidaikobo.com>
2018-10-16 21:02:25 +0900

    add Pages/Add.php

--- libs/a11yc/views/post/footer.php
+++ libs/a11yc/views/post/footer.php
@@ -3,7 +3,7 @@
 
 <footer id="a11yc_validator_footer">
 <?php
-	echo '<strong>A11yc <abbr title="Accessibility Validation Service">AVS</abbr></strong> - ';
+	echo '<strong>A11yc <abbr title="Accessibility Check Service">ACS</abbr></strong> - ';
 	echo 'Depends on <abbr title="Web Content Accessibility Guidelines">WCAG</abbr>2.0, JIS X 8341-3:2016, ISO/IEC 40500:2012. ';
 	echo 'Provided by <a href="http://www.jidaikobo.com">Jidaikobo Inc.</a>';
 ?>
