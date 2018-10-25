fd4b44ad511a12d3b537ef79ea4956e2628653af
shibata <shibata@jidaikobo.com>
2018-10-25 19:46:44 +0900

    move properties

--- libs/a11yc/classes/Model/Css.php
+++ libs/a11yc/classes/Model/Css.php
@@ -20,86 +20,6 @@ class Css
 	public static $suspicious_props = array();
 	public static $suspicious_val_prop = array();
 
-	protected static $vendors = array(
-		'-ms-', '-moz-', '-webkit-', '-o-', '-moz-osx-'
-	);
-
-	protected static $css_props = array(
-		'color', 'opacity', 'background', 'background-attachment', 'background-clip',
-		'background-color', 'background-image', 'background-origin', 'background-position',
-		'background-repeat', 'background-size', 'border', 'border-bottom',
-		'border-bottom-color', 'border-bottom-left-radius', 'border-bottom-right-radius',
-		'border-bottom-style', 'border-bottom-width', 'border-color', 'border-image',
-		'border-image-outset', 'border-image-repeat', 'border-image-slice',
-		'border-image-source', 'border-image-width', 'border-left', 'border-left-color',
-		'border-left-style', 'border-left-width', 'border-radius', 'border-right',
-		'border-right-color', 'border-right-style', 'border-right-width', 'border-style',
-		'border-top', 'border-top-color', 'border-top-left-radius',
-		'border-top-right-radius', 'border-top-style', 'border-top-width', 'border-width',
-		'box-decoration-break', 'box-shadow', 'image-resolution', 'object-fit',
-		'object-position', 'marquee-direction', 'marquee-play-count', 'marquee-speed',
-		'marquee-style', 'break-after', 'break-before', 'break-inside', 'column-count',
-		'column-fill', 'column-gap', 'column-rule', 'column-rule-color', 'column-rule-style',
-		'column-rule-width', 'column-span', 'column-width', 'columns', 'cue', 'cue-after',
-		'cue-before', 'pause', 'pause-after', 'pause-before', 'rest', 'rest-after',
-		'rest-before', 'speak', 'speak-as', 'voice-balance', 'voice-duration', 'voice-family',
-		'voice-pitch', 'voice-range', 'voice-rate', 'voice-stress', 'voice-volume',
-		'backface-visibility', 'perspective', 'perspective-origin',
-
-		'transform', 'transform-origin', 'transform-style', 'transition', 'transition-delay',
-		'transition-duration', 'transition-property', 'transition-timing-function',
-		'animation', 'animation-delay', 'animation-direction', 'animation-duration',
-		'animation-fill-mode', 'animation-iteration-count', 'animation-name',
-		'animation-play-state', 'animation-timing-function',
-
-		'align-content', 'align-items', 'align-self', 'flex', 'flex-basis', 'flex-direction',
-		'flex-flow', 'flex-grow', 'flex-shrink', 'flex-wrap', 'justify-content', 'order',
-		'font', 'font-family', 'font-feature-settings', 'font-kerning',
-		'font-language-override', 'font-size', 'font-size-adjust', 'font-stretch',
-		'font-style', 'font-synthesis', 'font-variant', 'font-variant-alternates',
-		'font-variant-caps', 'font-variant-east-asian', 'font-variant-ligatures',
-		'font-variant-numeric', 'font-variant-position', 'font-weight',
-
-		'fit', 'fit-position', 'image-orientation', 'orphans', 'page', 'page-break-after',
-		'page-break-before', 'page-break-inside', 'size', 'widows', 'hanging-punctuation',
-		'hyphens', 'letter-spacing', 'line-break', 'overflow-wrap', 'tab-size',
-
-		'text-align', 'text-align-last', 'text-decoration', 'text-decoration-color',
-		'text-decoration-line', 'text-decoration-skip', 'text-decoration-style',
-		'text-emphasis', 'text-emphasis-color', 'text-emphasis-position',
-		'text-emphasis-style', 'text-indent', 'text-justify', 'text-shadow',
-		'text-transform', 'text-underline-position', 'white-space', 'word-break',
-		'word-spacing', 'box-sizing', 'cursor', 'icon', 'ime-mode',
-
-		'nav-down', 'nav-index', 'nav-left', 'nav-right', 'nav-up', 'outline',
-		'outline-color', 'outline-offset', 'outline-style', 'outline-width',
-		'resize', 'text-overflow', 'direction', 'text-combine-horizontal',
-		'text-combine-mode', 'text-orientation', 'unicode-bidi', 'writing-mode',
-		'marks', 'grid-cell', 'grid-column', 'grid-column-align', 'grid-column-sizing',
-		'grid-column-span', 'grid-columns', 'grid-flow', 'grid-row', 'grid-row-align',
-		'grid-row-sizing', 'grid-row-span', 'grid-rows', 'grid-template', 'list-style',
-		'list-style-image', 'list-style-position', 'list-style-type', 'bottom', 'clip',
-		'left', 'position', 'right', 'top', 'z-index', 'border-collapse', 'border-spacing',
-		'caption-side', 'empty-cells', 'table-layout',
-
-		'clear', 'display', 'float', 'height', 'margin', 'margin-bottom', 'margin-left',
-		'margin-right', 'margin-top', 'max-height', 'max-width', 'min-height', 'min-width',
-		'overflow', 'overflow-style', 'overflow-x', 'overflow-y', 'padding',
-		'padding-bottom', 'padding-left', 'padding-right', 'padding-top',
-
-		'visibility', 'width', 'content', 'counter-increment', 'counter-reset', 'crop',
-		'move-to', 'page-policy', 'quotes', 'alignment-adjust', 'alignment-baseline',
-		'baseline-shift', 'dominant-baseline', 'drop-initial-after-adjust',
-		'drop-initial-after-align', 'drop-initial-before-adjust',
-		'drop-initial-before-align', 'drop-initial-size', 'drop-initial-value',
-		'inline-box-align', 'line-height', 'line-stacking', 'line-stacking-ruby',
-		'line-stacking-shift', 'line-stacking-strategy', 'text-height', 'vertical-align',
-		'ruby-align', 'ruby-overhang', 'ruby-position', 'ruby-span',
-		'target', 'target-name', 'target-new', 'target-position',
-
-		'filter', 'text-rendering', 'font-smoothing', 'appearance'
-	);
-
 	/**
 	 * fetch css
 	 *
@@ -267,25 +187,5 @@ class Css
 		}
 
 		return Util::urldec($url);
-	}
-
-	/**
-	 * props
-	 *
-	 * @return Array
-	 */
-	public static function props()
-	{
-		return self::$css_props;
-	}
-
-	/**
-	 * vendors
-	 *
-	 * @return Array
-	 */
-	public static function vendors()
-	{
-		return self::$vendors;
 	}
 }
--- libs/a11yc/classes/Model/Css/Format.php
+++ libs/a11yc/classes/Model/Css/Format.php
@@ -14,6 +14,87 @@ use A11yc\Model;
 
 class Format
 {
+
+	protected static $vendors = array(
+		'-ms-', '-moz-', '-webkit-', '-o-', '-moz-osx-'
+	);
+
+	protected static $css_props = array(
+		'color', 'opacity', 'background', 'background-attachment', 'background-clip',
+		'background-color', 'background-image', 'background-origin', 'background-position',
+		'background-repeat', 'background-size', 'border', 'border-bottom',
+		'border-bottom-color', 'border-bottom-left-radius', 'border-bottom-right-radius',
+		'border-bottom-style', 'border-bottom-width', 'border-color', 'border-image',
+		'border-image-outset', 'border-image-repeat', 'border-image-slice',
+		'border-image-source', 'border-image-width', 'border-left', 'border-left-color',
+		'border-left-style', 'border-left-width', 'border-radius', 'border-right',
+		'border-right-color', 'border-right-style', 'border-right-width', 'border-style',
+		'border-top', 'border-top-color', 'border-top-left-radius',
+		'border-top-right-radius', 'border-top-style', 'border-top-width', 'border-width',
+		'box-decoration-break', 'box-shadow', 'image-resolution', 'object-fit',
+		'object-position', 'marquee-direction', 'marquee-play-count', 'marquee-speed',
+		'marquee-style', 'break-after', 'break-before', 'break-inside', 'column-count',
+		'column-fill', 'column-gap', 'column-rule', 'column-rule-color', 'column-rule-style',
+		'column-rule-width', 'column-span', 'column-width', 'columns', 'cue', 'cue-after',
+		'cue-before', 'pause', 'pause-after', 'pause-before', 'rest', 'rest-after',
+		'rest-before', 'speak', 'speak-as', 'voice-balance', 'voice-duration', 'voice-family',
+		'voice-pitch', 'voice-range', 'voice-rate', 'voice-stress', 'voice-volume',
+		'backface-visibility', 'perspective', 'perspective-origin',
+
+		'transform', 'transform-origin', 'transform-style', 'transition', 'transition-delay',
+		'transition-duration', 'transition-property', 'transition-timing-function',
+		'animation', 'animation-delay', 'animation-direction', 'animation-duration',
+		'animation-fill-mode', 'animation-iteration-count', 'animation-name',
+		'animation-play-state', 'animation-timing-function',
+
+		'align-content', 'align-items', 'align-self', 'flex', 'flex-basis', 'flex-direction',
+		'flex-flow', 'flex-grow', 'flex-shrink', 'flex-wrap', 'justify-content', 'order',
+		'font', 'font-family', 'font-feature-settings', 'font-kerning',
+		'font-language-override', 'font-size', 'font-size-adjust', 'font-stretch',
+		'font-style', 'font-synthesis', 'font-variant', 'font-variant-alternates',
+		'font-variant-caps', 'font-variant-east-asian', 'font-variant-ligatures',
+		'font-variant-numeric', 'font-variant-position', 'font-weight',
+
+		'fit', 'fit-position', 'image-orientation', 'orphans', 'page', 'page-break-after',
+		'page-break-before', 'page-break-inside', 'size', 'widows', 'hanging-punctuation',
+		'hyphens', 'letter-spacing', 'line-break', 'overflow-wrap', 'tab-size',
+
+		'text-align', 'text-align-last', 'text-decoration', 'text-decoration-color',
+		'text-decoration-line', 'text-decoration-skip', 'text-decoration-style',
+		'text-emphasis', 'text-emphasis-color', 'text-emphasis-position',
+		'text-emphasis-style', 'text-indent', 'text-justify', 'text-shadow',
+		'text-transform', 'text-underline-position', 'white-space', 'word-break',
+		'word-spacing', 'box-sizing', 'cursor', 'icon', 'ime-mode',
+
+		'nav-down', 'nav-index', 'nav-left', 'nav-right', 'nav-up', 'outline',
+		'outline-color', 'outline-offset', 'outline-style', 'outline-width',
+		'resize', 'text-overflow', 'direction', 'text-combine-horizontal',
+		'text-combine-mode', 'text-orientation', 'unicode-bidi', 'writing-mode',
+		'marks', 'grid-cell', 'grid-column', 'grid-column-align', 'grid-column-sizing',
+		'grid-column-span', 'grid-columns', 'grid-flow', 'grid-row', 'grid-row-align',
+		'grid-row-sizing', 'grid-row-span', 'grid-rows', 'grid-template', 'list-style',
+		'list-style-image', 'list-style-position', 'list-style-type', 'bottom', 'clip',
+		'left', 'position', 'right', 'top', 'z-index', 'border-collapse', 'border-spacing',
+		'caption-side', 'empty-cells', 'table-layout',
+
+		'clear', 'display', 'float', 'height', 'margin', 'margin-bottom', 'margin-left',
+		'margin-right', 'margin-top', 'max-height', 'max-width', 'min-height', 'min-width',
+		'overflow', 'overflow-style', 'overflow-x', 'overflow-y', 'padding',
+		'padding-bottom', 'padding-left', 'padding-right', 'padding-top',
+
+		'visibility', 'width', 'content', 'counter-increment', 'counter-reset', 'crop',
+		'move-to', 'page-policy', 'quotes', 'alignment-adjust', 'alignment-baseline',
+		'baseline-shift', 'dominant-baseline', 'drop-initial-after-adjust',
+		'drop-initial-after-align', 'drop-initial-before-adjust',
+		'drop-initial-before-align', 'drop-initial-size', 'drop-initial-value',
+		'inline-box-align', 'line-height', 'line-stacking', 'line-stacking-ruby',
+		'line-stacking-shift', 'line-stacking-strategy', 'text-height', 'vertical-align',
+		'ruby-align', 'ruby-overhang', 'ruby-position', 'ruby-span',
+		'target', 'target-name', 'target-new', 'target-position',
+
+		'filter', 'text-rendering', 'font-smoothing', 'appearance'
+	);
+
 	/**
 	 * makeArray
 	 *
@@ -50,9 +131,9 @@ class Format
 		// remove vendor prefix
 		foreach (Model\Css::$suspicious_props as $k => $v)
 		{
-			foreach (Model\Css::props() as $prop)
+			foreach (self::$css_props as $prop)
 			{
-				foreach (Model\Css::vendors() as $vendor)
+				foreach (self::$vendors as $vendor)
 				{
 					if ($v == $vendor.$prop) unset(Model\Css::$suspicious_props[$k]);
 				}
@@ -176,7 +257,7 @@ class Format
 			if (empty($prop_and_vals)) continue;
 
 			// suspicious properties
-			if ( ! in_array($prop_and_vals[0], Model\Css::props()))
+			if ( ! in_array($prop_and_vals[0], self::$css_props))
 			{
 				Model\Css::$suspicious_props[] = $prop_and_vals[0];
 			}
