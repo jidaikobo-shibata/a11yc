afa0b6baa6df916aebf7f8ce057889d4138c6bb2
shibata <shibata@jidaikobo.com>
2018-10-25 21:12:46 +0900

    refactoring: Model\Bulk

--- libs/a11yc/classes/Model/Bulk.php
+++ libs/a11yc/classes/Model/Bulk.php
@@ -100,8 +100,6 @@ class Bulk
 	 */
 	public static function all()
 	{
-		$yml = Yaml::fetch();
-
 		// update all except for in trash item
 		$sql = 'SELECT * FROM '.A11YC_TABLE_PAGES.' WHERE `trash` = 0'.Db::currentVersionSql();
 
@@ -110,70 +108,13 @@ class Bulk
 			$url = $v['url'];
 
 			// results
-			foreach (Input::postArr('results') as $criterion => $vv)
-			{
-				$memo   = stripslashes($vv['memo']);
-				$uid    = intval(Arr::get($vv, 'uid', 0));
-				$result = intval(Arr::get($vv, 'result', 0));
-				$method = intval(Arr::get($vv, 'method', 0));
+			self::updateResults($url);
 
-				// add results
-				$sql = 'SELECT * FROM '.A11YC_TABLE_RESULTS.' WHERE';
-				$sql.= ' `url` = ? and `criterion` = ?'.Db::currentVersionSql().';';
-
-				if ( ! Db::fetch($sql, array($url, $criterion)))
-				{
-					$sql = 'INSERT INTO '.A11YC_TABLE_RESULTS;
-					$sql.= ' (`url`, `criterion`, `uid`, `memo`, `result`, `method`, `version`)';
-					$sql.= ' VALUES (?, ?, ?, ?, ?, ?, 0);';
-					Db::execute($sql, array($url, $criterion, $uid, $memo, $result, $method));
-				}
-
-				// force update all
-				if (Input::post('update_all') == 3)
-				{
-					$sql = 'UPDATE '.A11YC_TABLE_RESULTS;
-					$sql.= ' SET `memo` = ?, `uid` = ?, `result` = ?, `method` = ?';
-					$sql.= ' WHERE `criterion` = ? AND `url` = ? AND `version` = 0;';
-					Db::execute($sql, array($memo, $uid, $result, $method, $criterion, $url));
-				}
-			}
-
-			// update uncheck
-			if (Input::post('update_all') == 3)
-			{
-				$sql = 'UPDATE '.A11YC_TABLE_CHECKS;
-				$sql.= ' SET `is_checked` = ?';
-				$sql.= ' WHERE `url` = ?'.Db::currentVersionSql().';';
-				Db::execute($sql, array(false, $url));
-			}
+			// uncheck
+			self::updateUnchecks($url);
 
 			// checks
-			foreach (Input::postArr('chk') as $code => $vv)
-			{
-				// is_checked?
-				$sql = 'SELECT * FROM '.A11YC_TABLE_CHECKS.' WHERE';
-				$sql.= ' `url` = ? AND `code` = ?'.Db::currentVersionSql().';';
-				$result = Db::fetch($sql, array($url, $code));
-
-				$is_failure = ($yml['techs'][$code]['type'] == 'F');
-
-				// add new code
-				if ( ! $result)
-				{
-					$sql = 'INSERT INTO '.A11YC_TABLE_CHECKS;
-					$sql.= ' (`url`, `code`, `is_checked`, `is_failure`, `version`)';
-					$sql.= ' VALUES (?, ?, ?, ?, 0);';
-					Db::execute($sql, array($url, $code, true, $is_failure));
-				}
-				else
-				{
-					$sql = 'UPDATE '.A11YC_TABLE_CHECKS.' SET ';
-					$sql.= '`is_checked` = ?';
-					$sql.= ' WHERE `url` = ? AND `code` = ?'.Db::currentVersionSql().';';
-					Db::execute($sql, array(true, $url, $code));
-				}
-			}
+			self::updateChecks($url);
 
 			// update each page
 			$update_done = intval(Input::post('update_done'));
@@ -194,6 +135,97 @@ class Bulk
 
 			// update level
 			Pages::updateField($url, 'level', Evaluate::getLevelByUrl($url));
+		}
+	}
+
+	/**
+	 * udpate results
+	 *
+	 * @param String $url
+	 * @return Void
+	 */
+	private static function updateResults($url)
+	{
+		foreach (Input::postArr('results') as $criterion => $vv)
+		{
+			$memo   = stripslashes($vv['memo']);
+			$uid    = intval(Arr::get($vv, 'uid', 0));
+			$result = intval(Arr::get($vv, 'result', 0));
+			$method = intval(Arr::get($vv, 'method', 0));
+
+			// add results
+			$sql = 'SELECT * FROM '.A11YC_TABLE_RESULTS.' WHERE';
+			$sql.= ' `url` = ? and `criterion` = ?'.Db::currentVersionSql().';';
+
+			if ( ! Db::fetch($sql, array($url, $criterion)))
+			{
+				$sql = 'INSERT INTO '.A11YC_TABLE_RESULTS;
+				$sql.= ' (`url`, `criterion`, `uid`, `memo`, `result`, `method`, `version`)';
+				$sql.= ' VALUES (?, ?, ?, ?, ?, ?, 0);';
+				Db::execute($sql, array($url, $criterion, $uid, $memo, $result, $method));
+			}
+
+			// force update all
+			if (Input::post('update_all') == 3)
+			{
+				$sql = 'UPDATE '.A11YC_TABLE_RESULTS;
+				$sql.= ' SET `memo` = ?, `uid` = ?, `result` = ?, `method` = ?';
+				$sql.= ' WHERE `criterion` = ? AND `url` = ? AND `version` = 0;';
+				Db::execute($sql, array($memo, $uid, $result, $method, $criterion, $url));
+			}
+		}
+	}
+
+	/**
+	 * udpate unchecks
+	 *
+	 * @param String $url
+	 * @return Void
+	 */
+	private static function updateUnchecks($url)
+	{
+		if (Input::post('update_all') == 3)
+		{
+			$sql = 'UPDATE '.A11YC_TABLE_CHECKS;
+			$sql.= ' SET `is_checked` = ?';
+			$sql.= ' WHERE `url` = ?'.Db::currentVersionSql().';';
+			Db::execute($sql, array(false, $url));
+		}
+	}
+
+	/**
+	 * udpate checks
+	 *
+	 * @param String $url
+	 * @return Void
+	 */
+	private static function updateChecks($url)
+	{
+		$yml = Yaml::fetch();
+		foreach (Input::postArr('chk') as $code => $vv)
+		{
+			// is_checked?
+			$sql = 'SELECT * FROM '.A11YC_TABLE_CHECKS.' WHERE';
+			$sql.= ' `url` = ? AND `code` = ?'.Db::currentVersionSql().';';
+			$result = Db::fetch($sql, array($url, $code));
+
+			$is_failure = ($yml['techs'][$code]['type'] == 'F');
+
+			// add new code
+			if ( ! $result)
+			{
+				$sql = 'INSERT INTO '.A11YC_TABLE_CHECKS;
+				$sql.= ' (`url`, `code`, `is_checked`, `is_failure`, `version`)';
+				$sql.= ' VALUES (?, ?, ?, ?, 0);';
+				Db::execute($sql, array($url, $code, true, $is_failure));
+			}
+			else
+			{
+				$sql = 'UPDATE '.A11YC_TABLE_CHECKS.' SET ';
+				$sql.= '`is_checked` = ?';
+				$sql.= ' WHERE `url` = ? AND `code` = ?'.Db::currentVersionSql().';';
+				Db::execute($sql, array(true, $url, $code));
+			}
 		}
 	}
 }
