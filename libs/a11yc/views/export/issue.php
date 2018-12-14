3099163bade687028f5b3b6dbc1e86af0aa62b6e
有松 <arimatsu@jidaikobo.com>
2018-12-14 20:26:28 +0900

    css: export issue

--- libs/a11yc/views/export/issue.php
+++ libs/a11yc/views/export/issue.php
@@ -12,15 +12,17 @@
 	<link href="<?php echo A11YC_ASSETS_URL ?>/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
+<article>
 <?php
 
-$header = '<div class="issue_header">'.$title.'<br>'.A11YC_LANG_TEST_PERIOD.' '.$settings['test_period'].'</div>';
+$header = '<header class="issue_header">'.$title.'<br>'.A11YC_LANG_TEST_PERIOD.' '.$settings['test_period'].'</header>';
 
 // common cover page
 if (isset($issues[0]) && ($issues[0]['is_common'] || empty($issues[0]['url']))):
+//	echo '<section class="each_issue">';
+	echo '<div class="cover_page common">';
 	echo $header;
-	echo '<div class="cover_page">';
-	echo '<h1>'.A11YC_LANG_ISSUES_IS_COMMON.'</h1>';
+	echo '<h1 class="heading">'.A11YC_LANG_ISSUES_IS_COMMON.'</h1>';
 	echo '</div>';
 endif;
 ?>
@@ -30,28 +32,26 @@ endif;
 <?php
 // each cover page
 if ($k == 0 && isset($issues[0]) && ! empty($issues[0]['url'])):
+	echo '<section class="cover_page">';
 	echo $header;
-	echo '<div class="cover_page">';
-	echo '<h1>'.$issue['url'].'</h1>';
-	echo '</div>';
+	echo '<h1 class="heading">'.$issue['url'].'</h1>';
+	echo '</section>';
 endif;
 ?>
 
-<div class="each_page">
+<section class="each_page">
 
 <?php if ( ! empty($issue['image_path'])): ?>
-	<h2><?php echo A11YC_LANG_ISSUES_SCREENSHOT ?></h2>
+	<h2 class="heading"><?php echo A11YC_LANG_ISSUES_SCREENSHOT ?></h2>
 	<?php
-	echo '<div><img src="'.dirname(A11YC_URL).'/screenshots/'.$issue['id'].'/'.$issue['image_path'].'" alt="" /></div>';
+	echo '<div><img src="'.dirname(A11YC_URL).'/screenshots/'.$issue['id'].'/'.$issue['image_path'].'" alt="" class="screenshot"></div>';
 
 	?>
 <?php endif; ?>
 
-<table class="a11yc_table">
-
-<tr>
-	<th><?php echo A11YC_LANG_ISSUES_N_OR_E ?></th>
-	<td>
+<section class="issue">
+	<h3 class="heading"><?php echo A11YC_LANG_ISSUES_N_OR_E ?></h3>
+	<p>
 	<?php
 	if ($issue['n_or_e'] == 0):
 		echo 'Notice';
@@ -59,47 +59,31 @@ endif;
 		echo 'Error';
 	endif;
 	?>
-	</td>
-</tr>
+	</p>
 
-<tr>
-	<th><?php echo A11YC_LANG_ISSUES_HTML ?></th>
-	<td><?php echo $issue['html'] ?></td>
-</tr>
+	<h3 class="heading"><?php echo A11YC_LANG_ISSUES_HTML ?></h3>
+	<div>
+		<pre><code><?php echo $issue['html'] ?></code></pre>
+	</div>
 
-<tr>
-	<th><?php echo A11YC_LANG_ISSUES_ERRMSG ?></th>
-	<td><?php echo nl2br($issue['error_message']) ?></td>
-</tr>
-
-<tr>
-	<th><?php echo A11YC_LANG_ISSUES_STATUS ?></th>
-	<td>
-	<select name="status" id="a11yc_status">
-		<?php $selected = $issue['status'] == 0 ? ' selected="selected"': ''; ?>
-		<option<?php echo $selected ?> value="0"><?php echo A11YC_LANG_ISSUES_STATUS_1 ?></option>
-		<?php $selected = $issue['status'] == 1 ? ' selected="selected"': ''; ?>
-		<option<?php echo $selected ?> value="1"><?php echo A11YC_LANG_ISSUES_STATUS_2 ?></option>
-		<?php $selected = $issue['status'] == 2 ? ' selected="selected"': ''; ?>
-		<option<?php echo $selected ?> value="2"><?php echo A11YC_LANG_ISSUES_STATUS_3 ?></option>
-	</select>
-	</td>
-</tr>
+	<h3 class="heading"><?php echo A11YC_LANG_ISSUES_ERRMSG ?></h3>
+	<p>
+		<?php echo nl2br($issue['error_message']) ?>
+	</p>
 
 <?php if ($issue['tech_url']): ?>
-<tr>
-	<th><?php echo A11YC_LANG_ISSUES_TECH ?></th>
-	<td>
+	<h3 class="heading"><?php echo A11YC_LANG_ISSUES_TECH ?></h3>
+	<div>
 	<?php
 	foreach (explode("\n", $issue['tech_url']) as $tech_url):
 		echo '<a href="'.$tech_url.'">'.$tech_url.'</a>';
 	endforeach;
 	?>
-	</td>
-</tr>
+	</div>
 <?php endif; ?>
-</table>
-</div>
+
+</section><!-- /.each_page -->
 <?php endforeach; ?>
+</article>
 </body>
 </html>
