34e756926a028bcaeb62f8f8466f06c990be7591
有松 <arimatsu@jidaikobo.com>
2019-02-19 15:06:50 +0900

    css report header

--- views/inc_report_header.php
+++ views/inc_report_header.php
@@ -30,7 +30,11 @@ body > *
 {
 	max-width: 100%;
 }
-.a11yc_header
+body > article
+{
+	position: relative;
+}
+article:first-of-type .a11yc_header
 {
 	position: absolute;
 	top: 10px;
@@ -39,31 +43,68 @@ body > *
 	font-size: .7rem;
 	line-height: 1.5;
 }
+article:not(:first-of-type)
+{
+	margin-top: 5em;
+}
+article:not(:first-of-type) .a11yc_header
+{
+	display: none;
+}
+h2
+{
+	font-size: 1rem;
+	margin-top: 1.5em;
+	margin-bottom: .25em;
+}
+
 /* table */
 .a11yc_table {
 	border-collapse: collapse;
 	border: 1px solid #aaa;
-	border-bottom: none;
 	width: 100%;
+	font-size: .825em;
 }
-.a11yc_table:not(.a11yc_issues):not(.a11yc_table_report):not(.a11yc_setting) tr:nth-child(even),
-.a11yc_table:not(.a11yc_issues):not(.a11yc_table_report):not(.a11yc_setting) thead+tbody tr:nth-child(odd) {
+h1 + .a11yc_table 
+{
+	width: auto;
 }
-.a11yc_table:not(.a11yc_issues):not(.a11yc_table_report):not(.a11yc_setting) thead+tbody tr:nth-child(even) {
-	background-color: transparent;
+h1 + .a11yc_table th
+{
+	padding-right: 5mm;
 }
 .a11yc_table th,
 .a11yc_table td {
-	padding: 3px;
-	border-bottom: 1px solid #aaa;
 	vertical-align: top;
+	border-top: 1px solid #aaa;
+	border-bottom: 1px solid #aaa;
 }
 .a11yc_table th {
-	max-width: 14em;
 	min-width: 3em;
 	text-align: left;
 	white-space: nowrap;
 	word-break: break-all;
+}
+.a11yc_table .a11yc_result_string
+{
+	width: 45%;
+}
+h2 + .a11yc_table tr > :last-child
+{
+	width: 1px;
+}
+h2 + .a11yc_table  th,
+h2 + .a11yc_table  td
+{
+	padding: 1px 3px;
+}
+.a11yc_table thead tr > :nth-child(2),
+.a11yc_table tr > :nth-child(3),
+.a11yc_table tr > :nth-child(4),
+.a11yc_table tr > :nth-child(5)
+{
+	text-align: center;
+	white-space: nowrap;
 }
 .a11yc_table p {
 	margin-top: 0;
@@ -72,6 +113,9 @@ body > *
 .a11yc_table ul {
 	margin: 0;
 	padding-left: 20px;
+}
+.a11yc_table li {
+	line-height: 1.2;
 }
 @media print {
 	.noprint
@@ -82,9 +126,26 @@ body > *
 	{
 		font-size: .8em;
 	}
+	body
+	{
+		max-width: 190mm;
+	}
 	.a11yc_header
 	{
 		display: none !important;
+	}
+	body > article
+	{
+		page-break-before: always;
+		display: table;
+		width: 100%;
+		height: 290mm;
+		padding: 0 ;
+		min-height: auto;
+	}
+	article:not(:first-of-type)
+	{
+		margin-top: 0;
 	}
 	a:link,
 	a:visited
@@ -94,8 +155,8 @@ body > *
 }
 @page {
 	size: A4;
-	margin: 0;
-}
+	margin: 15mm 0 10mm;
+	}
 </style>
 
 </head>
