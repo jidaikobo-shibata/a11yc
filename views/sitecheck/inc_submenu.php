ff17fc2cd332840ccc88dcdb800800e6d6a27745
有松 <arimatsu@jidaikobo.com>
2019-03-01 19:13:10 +0900

    add p tag

--- views/sitecheck/inc_submenu.php
+++ views/sitecheck/inc_submenu.php
@@ -4,12 +4,13 @@ $selected_str = ' selected="selected"';
 ?>
 
 <form action="<?php echo A11YC_SITECHECK_URL ?>index" method="POST">
-
+<p>
 <label><?php echo A11YC_LANG_CTRL_KEYWORD_TITLE ?> <input type="text" name="keyword" size="20" value="<?php echo $keyword ?>"></label>
 
 <?php $checked = $use_re ? ' checked="checked"' : ''; ?>
 <label><input type="checkbox"<?php echo $checked ?> name="use_re" value="1"> <?php echo A11YC_LANG_USE_RE  ?></label>
-
+</p>
+<p>
 <label><?php echo A11YC_LANG_CTRL_CHECK ?><select name="op">
 	<?php $selected = Input::post('op') == 'contain_tabindex' ? $selected_str : '' ; ?>
 	<option value="contain_keyword"<?php echo $selected ?>><?php printf(A11YC_LANG_SITECHECK_CONTAINS, A11YC_LANG_CTRL_KEYWORD_TITLE) ?></option>
@@ -26,5 +27,6 @@ $selected_str = ' selected="selected"';
 	<?php $selected = Input::post('op') == 'contain_not_headings' ? $selected_str : '' ; ?>
 	<option value="contain_not_headings"<?php echo $selected ?>><?php printf(A11YC_LANG_SITECHECK_CONTAINS_NOT, "h*") ?></option>
 </select></label>
+</p>
 <input type="submit" name="str" value="<?php echo A11YC_LANG_CTRL_SEND ?>">
 </form>
