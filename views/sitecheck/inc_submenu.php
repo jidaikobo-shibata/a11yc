9727a088c5f00f3a0777bba72613ee4f10554321
shibata <shibata@jidaikobo.com>
2019-03-19 12:42:57 +0900

    fix typo

--- views/sitecheck/inc_submenu.php
+++ views/sitecheck/inc_submenu.php
@@ -28,7 +28,7 @@ $selected_str = ' selected="selected"';
 	<option value="contain_not_headings"<?php echo $selected ?>><?php printf(A11YC_LANG_SITECHECK_CONTAINS_NOT, "h*") ?></option>
 
 	<?php $selected = Input::post('op') == 'contain_not_href' ? $selected_str : '' ; ?>
-	<option value="contain_not_href"<?php echo $selected ?>><?php printf(A11YC_LANG_SITECHECK_CONTAINS_NOT, A11YC_LANG_SITECHECK_WITHOUT_HREF) ?></option>
+	<option value="contain_not_href"<?php echo $selected ?>><?php printf(A11YC_LANG_SITECHECK_CONTAINS, A11YC_LANG_SITECHECK_WITHOUT_HREF) ?></option>
 </select></label>
 </p>
 <input type="submit" name="str" value="<?php echo A11YC_LANG_CTRL_SEND ?>">
